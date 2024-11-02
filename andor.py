0a6f3e086493e38ff4f8122976fc228764dbf21c
Ayaan-Singh <asthana.divya@gmail.com>
2024-11-02 11:34:09 +0530

    hi

---
+++ andor.py
@@ -0,0 +1,13 @@
+b = 10
+a = 40
+c = -100
+
+if  (a < 100 and b < 10 and c < 0) :
+ print ("true")
+else :
+ print ("false")
+
+if  (a < 100 or b < 10 or c < 0) :
+  print ("true")
+else : 
+  print ("false")
